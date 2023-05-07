7e2e83c82759d96036c69c05edc6ef6402cf8ad2
Edo <edopuig@gmail.com>
2023-05-07 12:37:50 +0200

    pro config

--- src/main/java/com/appweb/psicologa/psicologa/component/DatabaseConfiguration.java
+++ src/main/java/com/appweb/psicologa/psicologa/component/DatabaseConfiguration.java
@@ -13,26 +13,27 @@ public class DatabaseConfiguration {
 
     @Autowired
     private Environment env;
-
+    
+    private String dbUrl = "jdbc:mysql://k5n8rw65ylm4rq8n:sui76j2oa7bbh7o7@j5zntocs2dn6c3fj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/ceifkuuc035429t8";
    
 
     @Bean
     public DataSource getDatasource(){
         
-        /*TEST*/
+        /*TEST
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
         dataSource.setUsername(env.getProperty("spring.datasource.name"));
         dataSource.setPassword(env.getProperty("spring.datasource.password"));
         dataSource.setUrl(env.getProperty("spring.datasource.url"));
-        return dataSource;
+        return dataSource;*/
 
          
-       /* 
-        private String dbUrl = "jdbc:mysql://k5n8rw65ylm4rq8n:sui76j2oa7bbh7o7@j5zntocs2dn6c3fj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/ceifkuuc035429t8";
+       /* PRO */
+       
        HikariConfig config = new HikariConfig();
         config.setJdbcUrl(dbUrl);
-        return new HikariDataSource(config);*/
+        return new HikariDataSource(config);
     }
     
 }
