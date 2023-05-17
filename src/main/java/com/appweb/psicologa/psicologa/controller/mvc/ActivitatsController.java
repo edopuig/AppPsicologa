bf705d7940f96b9ae731c1db9fa40e704e8dd8bb
Edo <edopuig@gmail.com>
2023-05-17 17:31:05 +0200

    POd i Xerr

--- src/main/java/com/appweb/psicologa/psicologa/controller/mvc/ActivitatsController.java
+++ src/main/java/com/appweb/psicologa/psicologa/controller/mvc/ActivitatsController.java
@@ -98,9 +98,7 @@ public class ActivitatsController {
     }
 
     /**
-     * ---------------------------------------------------------------- CURSOS
-     * ---------------------------------------------------------------------------
-     */
+     * ---------------------------------------------------- CURSOS----------------------------------------------------*/
 
     @GetMapping("/cursos")
     public ModelAndView getHomeCursos(@RequestParam(defaultValue = "all", required = false) String view_name,
@@ -173,7 +171,7 @@ public class ActivitatsController {
         return "redirect:/activitats/cursos";
     }
 
-  /* ----------------------------------------------- CURSOS ----------------------------------------------*/
+  /* ----------------------------------------------- Podcasts ----------------------------------------------*/
 
     @GetMapping("/podcasts")
     public ModelAndView getHomePodcasts(@RequestParam(defaultValue = "all", required = false) String view_name,
@@ -245,4 +243,78 @@ public class ActivitatsController {
         }
         return "redirect:/activitats/podcasts";
     }
+
+
+/* ----------------------------------------------- Xerrades ----------------------------------------------*/
+
+@GetMapping("/xerrades")
+public ModelAndView getHomeXerrades(@RequestParam(defaultValue = "all", required = false) String view_name,
+        @RequestParam(defaultValue = "0", required = false) int id) {
+
+    ModelAndView modelAndView = new ModelAndView("/podcasts");// Referencia al template blogs.html
+
+    Usuari usuariRegistrat = (Usuari) httpSession.getAttribute("usuariRegistrat"); // Agafem l'usuari que esta
+                                                                                   // registrat
+
+    switch (view_name) {
+        case "all":
+            modelAndView.addObject("activitats", activitatsRepository.buscarAllByTipus(4)); // Busquem totes els
+                                                                                            // blogs i les
+            // mostrem
+            break;
+        case "new":
+            if (usuariRegistrat != null && usuariRegistrat.getIdRol() == 1) {
+                modelAndView.addObject("activitatnova", new Activitats()); // Posem un blog buit per poder
+                // informala i crearla
+            } else { // Si no son usuaris i intentn entrar a la força, els obliguem a anar al login
+                     // directament.
+                modelAndView = new ModelAndView("/login");
+                modelAndView.addObject("usuari", new Usuari());
+            }
+
+            break;
+        case "update": // ----- De moment nomes la busca, si cliques guardar es fa una nova.
+            if (usuariRegistrat != null && usuariRegistrat.getIdRol() == 1) {
+                Activitats activitatModificar = activitatsRepository.buscarPerId(id);
+                modelAndView.addObject("activiatatUpdate", activitatModificar); // busca els blogs per la ID per
+                                                                                // poderla actualitzar
+                activitatModificar.setDescripcio(activitatModificar.getDescripcio().replace("<br>", "\n"));
+                break;
+            } else {
+                modelAndView = new ModelAndView("/login");
+                modelAndView.addObject("usuari", new Usuari());
+            }
+    }
+    return modelAndView;
+}
+
+@PostMapping("/xerrades")
+public String newAndUpdateXerrada(@ModelAttribute Activitats activitat) {
+    Usuari usuariRegistrat = (Usuari) httpSession.getAttribute("usuariRegistrat"); // Agafem l'usuari que esta
+                                                                                   // registrat
+    if (usuariRegistrat != null && usuariRegistrat.getIdRol() == 1) {
+
+        // Canviem els enters
+        activitat.setDescripcio(activitat.getDescripcio().replace("\n", "<br>"));
+        // posem el tipus d'activitat que es
+        activitat.setTipus(4);
+
+        if (activitat.getIdActivitat() > 0) {
+            activitatsRepository.update(activitat);
+        } else {
+            activitatsRepository.guardar(activitat);
+        }
+    }
+    return "redirect:/activitats/xerrades";
+}
+
+@DeleteMapping("/xerrades")
+public String eliminarXerradaById(@RequestParam int id) {
+    Usuari usuariRegistrat = (Usuari) httpSession.getAttribute("usuariRegistrat"); // Agafem l'usuari que esta
+                                                                                   // registrat
+    if (usuariRegistrat != null && usuariRegistrat.getIdRol() == 1) {
+        activitatsRepository.eliminarById(id);
+    }
+    return "redirect:/activitats/xerrades";
+}
 }
