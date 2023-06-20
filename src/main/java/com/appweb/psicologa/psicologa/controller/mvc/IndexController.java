32b880ab4f5a32589511edc001fa86548214911c
Edo <edopuig@gmail.com>
2023-06-20 16:50:02 +0200

    Parche error

---
+++ src/main/java/com/appweb/psicologa/psicologa/controller/mvc/IndexController.java
@@ -0,0 +1,17 @@
+package com.appweb.psicologa.psicologa.controller.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+    
+    @GetMapping
+    public ModelAndView getHome(){
+        return new ModelAndView("/index");
+    }
+}
